8b2a619b92830e75bb09755b1dec9093d2d31ab3
maitungmn <goingdownfr@gmail.com>
2019-07-30 21:15:39 +0700

    fix gitignore

--- .history/src/components/Profile/Profile_20190730190309.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Profile = ({ isProfileOpen, toggleModal }) => {
-  return <div>{"CheckCheck"}</div>;
-};
--- .history/src/components/Profile/Profile_20190730190326.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Profile = ({ isProfileOpen, toggleModal }) => {
-  return <div>{"CheckCheck"}</div>;
-};
-
-export default Profile;
